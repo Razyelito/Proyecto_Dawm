316c7f23fbf169959b2021ea8e6d7b9bbb325ae5
JohannaLopez <[Your>
2022-01-29 00:19:05 -0500

    Modificacion del archivo help

--- src/app/noticias/interface/noticia.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Noticia{
-    id:string;
-    title:string;
-    description:string;
-    published_date:string;
-    img: string;
-    description_short:string;
-}
--- src/app/talleres/interface/talleres.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Taller{
-    id: string,
-    title: string,
-    title_detalle: string,
-    description: string,
-    date: string,
-    img: string ,
-    description_short:string
-}
